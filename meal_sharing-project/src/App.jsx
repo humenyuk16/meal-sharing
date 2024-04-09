1bf1473e6deade0c53c7cc0dc8e75b53f1efe024
Anastasiia <humenyuknastia.9616@gmail.com>
2024-04-09 23:12:00 +0200

    react2 week1 + react2 week2

---
+++ meal_sharing-project/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MealsList from "./components/MealsList";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <MealsList></MealsList>
+    </div>
+  );
+}
+
+export default App;
